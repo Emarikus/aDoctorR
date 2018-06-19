feebcc3c110b38fef72b5a856eec1061e15e7c01
Emarikus <emaiannone@hotmail.it>
2018-06-19 13:27:11 +0200

    Test Commit #3

---
+++ src/TestClass.java
@@ -0,0 +1,10 @@
+import dialog.DialogUI;
+import org.eclipse.jdt.internal.compiler.ast.MethodDeclaration;
+import parser.CodeParser;
+
+public class TestClass {
+    public static void main(String[] args)  {
+        CodeParser cp;
+    }
+
+}
